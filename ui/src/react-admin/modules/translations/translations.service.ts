7f16c69631aaa0c713a1825dd23a612917ec6d36
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-11-24 17:02:42 +0100

    feat(avo-2210): convert translation graphql queries to rest endpoints

--- ui/src/react-admin/modules/translations/translations.service.ts
+++ ui/src/react-admin/modules/translations/translations.service.ts
@@ -1,48 +1,17 @@
 import { CustomError } from '~modules/shared/helpers/custom-error';
-import { dataService } from '~modules/shared/services/data-service';
-import { AvoOrHetArchief } from '~modules/shared/types';
+import { fetchWithLogoutJson } from '~modules/shared/helpers/fetch-with-logout';
 import { AdminConfigManager } from '~core/config';
-import { TranslationQueryTypes, TRANSLATIONS_QUERIES } from './queries/translations.queries';
 
 export class TranslationsService {
-	private static getQueries() {
-		return TRANSLATIONS_QUERIES[
-			AdminConfigManager.getConfig().database.databaseApplicationType
-		];
+	private static getBaseUrl(): string {
+		return `${AdminConfigManager.getConfig().database.proxyUrl}/translations`;
 	}
 
 	static async fetchTranslations(): Promise<{ name: string; value: Record<string, string> }[]> {
 		try {
-			// retrieve translations
-			const response = await dataService.query<
-				TranslationQueryTypes['GetTranslationsQuery'],
-				TranslationQueryTypes['GetTranslationsQueryVariables']
-			>({
-				query: this.getQueries().GetTranslationsDocument,
-			});
-
-			if (
-				AdminConfigManager.getConfig().database.databaseApplicationType ===
-				AvoOrHetArchief.avo
-			) {
-				return (
-					(response as TranslationQueryTypes['GetTranslationsQueryAvo'])
-						.app_site_variables || null
-				);
-			} else {
-				return (
-					(response as TranslationQueryTypes['GetTranslationsQueryHetArchief'])
-						.app_config || null
-				);
-			}
+			return fetchWithLogoutJson(this.getBaseUrl());
 		} catch (err) {
-			const error = new CustomError('Failed to fetch translations', err, {
-				query: 'GET_TRANSLATIONS',
-			});
-
-			console.error(error);
-
-			throw error;
+			throw new CustomError('Failed to fetch translations', err);
 		}
 	}
 
@@ -53,29 +22,19 @@ export class TranslationsService {
 	 */
 	static async updateTranslations(name: string, translations: any) {
 		try {
-			await dataService.query<
-				TranslationQueryTypes['UpdateTranslationsMutation'],
-				TranslationQueryTypes['UpdateTranslationsMutationVariables']
-			>({
-				query: this.getQueries().UpdateTranslationsDocument,
-				variables: {
-					name,
-					translations,
-				},
+			await fetchWithLogoutJson(this.getBaseUrl(), {
+				method: 'POST',
+				body: JSON.stringify({
+					key: name,
+					data: translations,
+				}),
 			});
 			await AdminConfigManager.getConfig().services.queryCache.clear('clearTranslations');
 		} catch (err) {
-			const error = new CustomError('Failed to update translations', err, {
-				query: 'UPDATE_TRANSLATIONS',
-				variables: {
-					name,
-					translations,
-				},
+			throw new CustomError('Failed to update translations', err, {
+				key: name,
+				data: translations,
 			});
-
-			console.error(error);
-
-			throw error;
 		}
 	}
 }
