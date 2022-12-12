e12bc50840868e9d9d5d1437144cb48adbc7307b
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-12-12 15:44:59 +0100

    fix(users): use null coalescing instead of || operator for defaults

--- api/src/modules/users/users.converters.ts
+++ api/src/modules/users/users.converters.ts
@@ -31,10 +31,10 @@ export function convertUserInfoToCommonUser(
 		const profile = userInfo as ProfileHetArchief;
 		return {
 			profileId: profile.id,
-			email: profile.mail || undefined,
-			firstName: profile.first_name || undefined,
-			lastName: profile.last_name || undefined,
-			fullName: profile.full_name || undefined,
+			email: profile.mail ?? undefined,
+			firstName: profile.first_name ?? undefined,
+			lastName: profile.last_name ?? undefined,
+			fullName: profile.full_name ?? undefined,
 			userGroup: {
 				id: profile.group?.id,
 				name: profile.group?.name,
@@ -45,7 +45,7 @@ export function convertUserInfoToCommonUser(
 			),
 			organisation: {
 				name:
-					profile.maintainer_users_profiles?.[0]?.maintainer.schema_name ||
+					profile.maintainer_users_profiles?.[0]?.maintainer.schema_name ??
 					undefined,
 				or_id:
 				profile.maintainer_users_profiles?.[0]?.maintainer.schema_identifier,
@@ -61,39 +61,39 @@ export function convertUserInfoToCommonUser(
 			const user = userInfo as Avo.User.User;
 			return {
 				profileId: user.profile.id,
-				stamboek: user.profile.stamboek || undefined,
+				stamboek: user.profile.stamboek ?? undefined,
 				organisation: user.profile?.organisation,
-				educationalOrganisations: (user.profile?.organizations || []).map(
+				educationalOrganisations: (user.profile?.organizations ?? []).map(
 					(org): Avo.EducationOrganization.Organization => ({
 						organizationId: undefined,
-						unitId: org?.unitAddress || null,
+						unitId: org?.unitAddress ?? null,
 						label: org?.organizationName,
 					}),
 				),
 				subjects: user.profile.subjects,
 				educationLevels: user.profile.educationLevels,
-				isException: user.profile.is_exception || undefined,
-				businessCategory: user.profile.business_category || undefined,
+				isException: user.profile.is_exception ?? undefined,
+				businessCategory: user.profile.business_category ?? undefined,
 				createdAt: user.profile.created_at,
 				userGroup: {
 					name: undefined,
 					label: undefined,
-					id: user.profile.userGroupIds[0] || undefined,
+					id: user.profile.userGroupIds[0] ?? undefined,
 				},
 				userId: user.uid,
 				uid: user.uid,
-				email: user.mail || undefined,
-				fullName: user.full_name || undefined,
-				firstName: user.first_name || undefined,
-				lastName: user.last_name || undefined,
-				isBlocked: user.is_blocked || undefined,
+				email: user.mail ?? undefined,
+				fullName: user.full_name ?? undefined,
+				firstName: user.first_name ?? undefined,
+				lastName: user.last_name ?? undefined,
+				isBlocked: user.is_blocked ?? undefined,
 				blockedAt: undefined,
 				unblockedAt: undefined,
 				lastAccessAt: user.last_access_at,
 				tempAccess: user?.temp_access
 					? {
-						from: user?.temp_access?.from || null,
-						until: user?.temp_access?.until || null,
+						from: user?.temp_access?.from ?? null,
+						until: user?.temp_access?.until ?? null,
 						status: isNil(user?.temp_access?.current?.status)
 							? null
 							: user?.temp_access?.current?.status === 1,
@@ -106,9 +106,9 @@ export function convertUserInfoToCommonUser(
 			const profile = userInfo as Avo.User.Profile;
 			return {
 				profileId: profile.id,
-				stamboek: profile.stamboek || undefined,
+				stamboek: profile.stamboek ?? undefined,
 				organisation: profile.organisation,
-				educationalOrganisations: (profile.organizations || []).map(
+				educationalOrganisations: (profile.organizations ?? []).map(
 					(org): Avo.EducationOrganization.Organization => ({
 						organizationId: undefined,
 						unitId: undefined,
@@ -117,28 +117,28 @@ export function convertUserInfoToCommonUser(
 				),
 				subjects: profile.subjects,
 				educationLevels: profile.educationLevels,
-				isException: profile.is_exception || undefined,
-				businessCategory: profile.business_category || undefined,
+				isException: profile.is_exception ?? undefined,
+				businessCategory: profile.business_category ?? undefined,
 				createdAt: profile.created_at,
 				userGroup: {
 					name:  undefined,
 					label:  undefined,
-					id: profile.userGroupIds[0] || undefined,
+					id: profile.userGroupIds[0] ?? undefined,
 				},
 				userId: profile.user_id,
 				uid: profile.user_id,
-				email: profile?.user?.mail || undefined,
-				fullName: profile?.user?.full_name || undefined,
-				firstName: profile?.user?.first_name || undefined,
-				lastName: profile?.user?.last_name || undefined,
-				isBlocked: profile?.user?.is_blocked || undefined,
+				email: profile?.user?.mail ?? undefined,
+				fullName: profile?.user?.full_name ?? undefined,
+				firstName: profile?.user?.first_name ?? undefined,
+				lastName: profile?.user?.last_name ?? undefined,
+				isBlocked: profile?.user?.is_blocked ?? undefined,
 				blockedAt: undefined,
 				unblockedAt: undefined,
 				lastAccessAt: profile?.user?.last_access_at,
 				tempAccess: profile?.user?.temp_access
 					? {
-						from: profile?.user?.temp_access?.from || null,
-						until: profile?.user?.temp_access?.until || null,
+						from: profile?.user?.temp_access?.from ?? null,
+						until: profile?.user?.temp_access?.until ?? null,
 						status: isNil(profile?.user?.temp_access?.current?.status)
 							? null
 							: profile?.user?.temp_access?.current?.status === 1,
@@ -151,45 +151,45 @@ export function convertUserInfoToCommonUser(
 			const user = userInfo as ProfileAvo;
 			return {
 				profileId: user.profile_id,
-				stamboek: user.stamboek || undefined,
+				stamboek: user.stamboek ?? undefined,
 				organisation: user.company_name
 					? ({
 						name: user.company_name,
 					} as Avo.Organization.Organization)
 					: undefined,
-				educationalOrganisations: (user.organisations || []).map(
+				educationalOrganisations: (user.organisations ?? []).map(
 					(org): Avo.EducationOrganization.Organization => ({
 						organizationId: org.organization_id,
-						unitId: org.unit_id || null,
-						label: org.organization?.ldap_description || '',
+						unitId: org.unit_id ?? null,
+						label: org.organization?.ldap_description ?? '',
 					}),
 				),
 				subjects: user.classifications?.map(
 					(classification) => classification.key,
 				),
 				educationLevels: user.contexts?.map((context) => context.key),
-				isException: user.is_exception || undefined,
-				businessCategory: user.business_category || undefined,
+				isException: user.is_exception ?? undefined,
+				businessCategory: user.business_category ?? undefined,
 				createdAt: user.acc_created_at,
 				userGroup: {
-					name: user.group_name || undefined,
-					label: user.group_name || undefined,
-					id: user.group_id || undefined,
+					name: user.group_name ?? undefined,
+					label: user.group_name ?? undefined,
+					id: user.group_id ?? undefined,
 				},
 				userId: user.user_id,
 				uid: user.user_id,
-				email: user.mail || undefined,
-				fullName: user.full_name || undefined,
-				firstName: user.first_name || undefined,
-				lastName: user.last_name || undefined,
-				isBlocked: user.is_blocked || undefined,
+				email: user.mail ?? undefined,
+				fullName: user.full_name ?? undefined,
+				firstName: user.first_name ?? undefined,
+				lastName: user.last_name ?? undefined,
+				isBlocked: user.is_blocked ?? undefined,
 				blockedAt: user?.blocked_at?.date,
 				unblockedAt: user?.unblocked_at?.date,
 				lastAccessAt: user.last_access_at,
 				tempAccess: user?.user?.temp_access
 					? {
-						from: user?.user?.temp_access?.from || null,
-						until: user?.user?.temp_access?.until || null,
+						from: user?.user?.temp_access?.from ?? null,
+						until: user?.user?.temp_access?.until ?? null,
 						status: isNil(user?.user?.temp_access?.current?.status)
 							? null
 							: user?.user?.temp_access?.current?.status === 1,
