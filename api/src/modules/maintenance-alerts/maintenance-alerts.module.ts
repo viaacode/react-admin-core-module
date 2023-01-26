31e9b83f0219a9952f1d73693eca99ed37ba32e4
Brecht Tourlousse <brecht.tourlousse@studiohyperdrive.be>
2023-01-26 13:22:54 +0100

    feat(maintenance-alerts): setup module

---
+++ api/src/modules/maintenance-alerts/maintenance-alerts.module.ts
@@ -0,0 +1,14 @@
+
+import { Module } from "@nestjs/common";
+
+import { DataModule } from "../data";
+import { MaintenanceAlertsController } from "./controllers/maintenance-alerts.controller";
+import { MaintenanceAlertsService } from "./services/maintenance-alerts.service";
+
+@Module({
+	controllers: [MaintenanceAlertsController],
+	providers: [MaintenanceAlertsService],
+	imports: [DataModule],
+	exports: [MaintenanceAlertsService],
+})
+export class MaintenanceAlertsModule {}
