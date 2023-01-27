dac9f678066f78c9b72e6c86d2ef7a8b39db2ac9
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-01-27 20:06:45 +0100

    fix(export): avoid using path aliases in export files
    since i think it might be the reason types are not correctly autocompleted in the client

--- ui/src/react-admin/modules/index.ts
+++ ui/src/react-admin/modules/index.ts
@@ -1,7 +1,7 @@
-import { AdminConfigManager } from '~core/config/config.class';
+import { AdminConfigManager } from '../core/config';
 
 // Admin Core
-export type { AdminConfig } from '~core/config/config.types';
+export type { AdminConfig } from '../core/config/config.types';
 export { AdminConfigManager };
 export {
 	fetchWithLogoutJson,
@@ -12,14 +12,14 @@ export {
 // Content pages
 export { default as ContentPageOverview } from './content-page/views/ContentPageOverview';
 export { default as ContentPageDetail } from './content-page/views/ContentPageDetail';
-export { default as ContentPageEdit } from '~modules/content-page/views/ContentPageEdit';
-export { default as ContentPageRenderer } from '~modules/content-page/components/ContentPageRenderer/ContentPageRenderer';
+export { default as ContentPageEdit } from './content-page/views/ContentPageEdit';
+export { default as ContentPageRenderer } from './content-page/components/ContentPageRenderer/ContentPageRenderer';
 export type { ColorOption } from './content-page/components/fields/ColorSelect/ColorSelect';
-export type { ContentPageInfo } from '~modules/content-page/types/content-pages.types';
-export type { DbContentPage } from '~modules/content-page/types/content-pages.types';
-export type { ContentPageLabel } from '~modules/content-page/types/content-pages.types';
-export type { ContentPageDetailProps } from '~modules/content-page/views/ContentPageDetail';
-export type { ContentPageEditProps } from '~modules/content-page/views/ContentPageEdit';
+export type { ContentPageInfo } from './content-page/types/content-pages.types';
+export type { DbContentPage } from './content-page/types/content-pages.types';
+export type { ContentPageLabel } from './content-page/types/content-pages.types';
+export type { ContentPageDetailProps } from './content-page/views/ContentPageDetail';
+export type { ContentPageEditProps } from './content-page/views/ContentPageEdit';
 export type {
 	ContentBlockConfig,
 	ContentBlockErrors,
@@ -33,15 +33,15 @@ export type {
 } from './content-page/types/content-block.types';
 export { ContentBlockEditor } from './content-page/types/content-block.types';
 export { CONTENT_BLOCK_CONFIG_MAP } from './content-page/const/content-block-config-map';
-export { Color } from '~modules/content-page/types/content-block.types';
-export { ContentBlockType } from '~modules/content-page/types/content-block.types';
-export { PublishOption } from '~modules/content-page/types/content-pages.types';
-export { ContentPageService } from '~modules/content-page/services/content-page.service';
+export { Color } from './content-page/types/content-block.types';
+export { ContentBlockType } from './content-page/types/content-block.types';
+export { PublishOption } from './content-page/types/content-pages.types';
+export { ContentPageService } from './content-page/services/content-page.service';
 export * from './content-page/components/blocks';
 export {
 	convertDbContentPageToContentPageInfo,
 	convertDbContentPagesToContentPageInfos,
-} from '~modules/content-page/services/content-page.converters';
+} from './content-page/services/content-page.converters';
 export { GET_ALIGN_OPTIONS } from './content-page/const/get-align-options';
 export { GET_BUTTON_TYPE_OPTIONS } from './content-page/const/get-button-type-options';
 export { GET_HEADING_TYPE_OPTIONS } from './content-page/const/get-heading-type-options';
@@ -51,24 +51,24 @@ export { ContentPageLabelService } from './content-page-labels/content-page-labe
 
 // Navigation
 export { default as NavigationOverview } from './navigation/views/NavigationOverview';
-export { default as NavigationEdit } from '~modules/navigation/views/NavigationEdit';
-export { default as NavigationDetail } from '~modules/navigation/views/NavigationDetail';
+export { default as NavigationEdit } from './navigation/views/NavigationEdit';
+export { default as NavigationDetail } from './navigation/views/NavigationDetail';
 
-export { UserDetail } from '~modules/user/views/UserDetail';
-export { UserEdit } from '~modules/user/views/UserEdit';
-export { UserOverview } from '~modules/user/views/UserOverview';
+export { UserDetail } from './user/views/UserDetail';
+export { UserEdit } from './user/views/UserEdit';
+export { UserOverview } from './user/views/UserOverview';
 
-export type { CommonUser } from '~modules/user/user.types';
+export type { CommonUser } from './user/user.types';
 
 // User groups and permissions
-export { default as UserGroupOverview } from '~modules/user-group/views/UserGroupOverview';
+export { default as UserGroupOverview } from './user-group/views/UserGroupOverview';
 
 // Translations
-export { default as TranslationsOverview } from '~modules/translations/views/TranslationsOverview';
+export { default as TranslationsOverview } from './translations/views/TranslationsOverview';
 
 // Helpers
 export { sanitizeHtml } from './shared/helpers/sanitize/index';
 export { SanitizePreset } from './shared/helpers/sanitize/presets';
 
 // STOPGAP
-export type { DefaultComponentProps, ValueOf } from '~modules/shared';
+export type { DefaultComponentProps, ValueOf } from './shared';
