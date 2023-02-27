a06fdf1fdabbafbd86c8cd6afada85de1b75f88b
Emile Van Tichelen <emile.vantichelen@student.hogent.be>
2023-02-27 16:13:04 +0100

    feat(mainenance-alerts): add MaintenanceAlertsDto test

--- api/src/modules/maintenance-alerts/dto/maintenance-alerts.dto.spec.ts
+++ api/src/modules/maintenance-alerts/dto/maintenance-alerts.dto.spec.ts
@@ -1,13 +1,33 @@
-import { MaintenanceAlertsQueryDto } from "./maintenance-alerts.dto";
+import {
+	CreateMaintenanceAlertDto,
+	MaintenanceAlertsQueryDto,
+	UpdateMaintenanceAlertDto,
+} from './maintenance-alerts.dto';
 
 describe('MaintenanceAlertsDto', () => {
-	it('should be able to construct a MaintenanceAlertsQueryDto', () => {
-		const maintenanceAlertsQueryDto = new MaintenanceAlertsQueryDto();
-		expect(maintenanceAlertsQueryDto).toEqual({
-			page: 1,
-			size: 10,
-			orderProp: 'fromDate',
-			orderDirection: 'desc',
+	describe('MaintenanceAlertsQueryDto', () => {
+		it('should be able to construct a MaintenanceAlertsQueryDto object', () => {
+			const maintenanceAlertsQueryDto = new MaintenanceAlertsQueryDto();
+			expect(maintenanceAlertsQueryDto).toEqual({
+				page: 1,
+				size: 10,
+				orderProp: 'fromDate',
+				orderDirection: 'desc',
+			});
+		});
+	});
+
+	describe('CreateMaintenanceAlertDto', () => {
+		it('should be able to construct a CreateMaintenanceAlertDto object', () => {
+			const createMaintenanceAlertDto = new CreateMaintenanceAlertDto();
+			expect(createMaintenanceAlertDto).toEqual({});
+		});
+	});
+
+	describe('UpdateMaintenanceAlertDto', () => {
+		it('should be able to construct a UpdateMaintenanceAlertDto object', () => {
+			const updateMaintenanceAlertDto = new UpdateMaintenanceAlertDto();
+			expect(updateMaintenanceAlertDto).toEqual({});
 		});
 	});
 });
