45dceac694965ddad068452d0da4ec6b528283ea
Emile Van Tichelen <emile.vantichelen@student.hogent.be>
2023-03-01 08:47:16 +0100

    feat(maintenance-alerts): Add test Delete method

--- api/src/modules/maintenance-alerts/mocks/maintenance-alerts.mocks.ts
+++ api/src/modules/maintenance-alerts/mocks/maintenance-alerts.mocks.ts
@@ -2,66 +2,78 @@ import { Group, GroupIdToName, HetArchiefUser } from '../../users';
 import { Idp } from '../../shared/auth/auth.types';
 import { PermissionName } from '@viaa/avo2-types';
 
-import { MaintenanceAlert, MaintenanceAlertType } from "../maintenance-alerts.types";
-import { FindMaintenanceAlertByIdQuery, FindMaintenanceAlertsQuery } from '../../../modules/shared/generated/graphql-db-types-hetarchief';
+import {
+	MaintenanceAlert,
+	MaintenanceAlertType,
+} from '../maintenance-alerts.types';
+import {
+	FindMaintenanceAlertByIdQuery,
+	FindMaintenanceAlertsQuery,
+} from '../../../modules/shared/generated/graphql-db-types-hetarchief';
 
-export const mockGqlMaintenanceAlert1: FindMaintenanceAlertsQuery['app_maintenance_alerts'][0] = {
-	id: "29f9eac3-0c7e-48ec-9216-3a3af7487766",
-	title: "Gepland onderhoud",
-	message: "Opgelet! Tussen 25 en 27 februari plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-	type: MaintenanceAlertType.QUESTION as any,
-	from_date: "2022-02-25T16:36:06.045845",
-	until_date: "2022-02-27T16:36:06.045845",
-	user_groups: [
-		"0213c8d4-f459-45ef-8bbc-96268ab56d01",
-		"04150e6e-b779-4125-84e5-6ee6fc580757",
-		"0b281484-76cd-45a9-b6ce-68a0ea7f4b26",
-		"c56d95aa-e918-47ca-b102-486c9449fc4a"
-	]
-}
+export const mockGqlMaintenanceAlert1: FindMaintenanceAlertsQuery['app_maintenance_alerts'][0] =
+	{
+		id: '29f9eac3-0c7e-48ec-9216-3a3af7487766',
+		title: 'Gepland onderhoud',
+		message:
+			'Opgelet! Tussen 25 en 27 februari plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		type: MaintenanceAlertType.QUESTION as any,
+		from_date: '2022-02-25T16:36:06.045845',
+		until_date: '2022-02-27T16:36:06.045845',
+		user_groups: [
+			'0213c8d4-f459-45ef-8bbc-96268ab56d01',
+			'04150e6e-b779-4125-84e5-6ee6fc580757',
+			'0b281484-76cd-45a9-b6ce-68a0ea7f4b26',
+			'c56d95aa-e918-47ca-b102-486c9449fc4a',
+		],
+	};
 
-export const mockGqlMaintenanceAlert2: FindMaintenanceAlertByIdQuery['app_maintenance_alerts'][0] = {
-	id: "17741a72-807e-43d0-9869-176593171938",
-  title: "Gepland onderhoud",
-	message: "Opgelet! Tussen 25 en 27 maart plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-	type: MaintenanceAlertType.EXCLAMATION as any,
-	from_date: "2022-02-25T16:36:06.045845",
-	until_date: "2022-02-27T16:36:06.045845",
-	user_groups: [
-		"0213c8d4-f459-45ef-8bbc-96268ab56d01",
-		"04150e6e-b779-4125-84e5-6ee6fc580757",
-		"0b281484-76cd-45a9-b6ce-68a0ea7f4b26",
-		"c56d95aa-e918-47ca-b102-486c9449fc4a",
-	]
-}
+export const mockGqlMaintenanceAlert2: FindMaintenanceAlertByIdQuery['app_maintenance_alerts'][0] =
+	{
+		id: '17741a72-807e-43d0-9869-176593171938',
+		title: 'Gepland onderhoud',
+		message:
+			'Opgelet! Tussen 25 en 27 maart plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		type: MaintenanceAlertType.EXCLAMATION as any,
+		from_date: '2022-02-25T16:36:06.045845',
+		until_date: '2022-02-27T16:36:06.045845',
+		user_groups: [
+			'0213c8d4-f459-45ef-8bbc-96268ab56d01',
+			'04150e6e-b779-4125-84e5-6ee6fc580757',
+			'0b281484-76cd-45a9-b6ce-68a0ea7f4b26',
+			'c56d95aa-e918-47ca-b102-486c9449fc4a',
+		],
+	};
 
 export const mockMaintenanceAlert1: MaintenanceAlert = {
-	id: "29f9eac3-0c7e-48ec-9216-3a3af7487766",
-	title: "Gepland onderhoud",
-	message: "Opgelet! Tussen 25 en 27 januari plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+	id: '29f9eac3-0c7e-48ec-9216-3a3af7487766',
+	title: 'Gepland onderhoud',
+	message:
+		'Opgelet! Tussen 25 en 27 januari plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
 	type: MaintenanceAlertType.QUESTION,
-	fromDate: "2022-02-25T16:36:06.045845",
-	untilDate: "2022-02-27T16:36:06.045845",
+	fromDate: '2022-02-25T16:36:06.045845',
+	untilDate: '2022-02-27T16:36:06.045845',
 	userGroups: [
-		"0213c8d4-f459-45ef-8bbc-96268ab56d01",
-		"04150e6e-b779-4125-84e5-6ee6fc580757",
-		"0b281484-76cd-45a9-b6ce-68a0ea7f4b26",
-		"c56d95aa-e918-47ca-b102-486c9449fc4a",
-	]
+		'0213c8d4-f459-45ef-8bbc-96268ab56d01',
+		'04150e6e-b779-4125-84e5-6ee6fc580757',
+		'0b281484-76cd-45a9-b6ce-68a0ea7f4b26',
+		'c56d95aa-e918-47ca-b102-486c9449fc4a',
+	],
 };
 const mockMaintenanceAlert2: MaintenanceAlert = {
-	id: "17741a72-807e-43d0-9869-176593171938",
-  title: "Gepland onderhoud",
-	message: "Opgelet! Tussen 25 en 27 maart plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+	id: '17741a72-807e-43d0-9869-176593171938',
+	title: 'Gepland onderhoud',
+	message:
+		'Opgelet! Tussen 25 en 27 maart plannen we een onderhoud aan Het archief. Je zal dus tijdenlijk niet kunnen inloggen Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
 	type: MaintenanceAlertType.EXCLAMATION,
-	fromDate: "2022-02-25T16:36:06.045845",
-	untilDate: "2022-02-27T16:36:06.045845",
+	fromDate: '2022-02-25T16:36:06.045845',
+	untilDate: '2022-02-27T16:36:06.045845',
 	userGroups: [
-		"0213c8d4-f459-45ef-8bbc-96268ab56d01",
-		"04150e6e-b779-4125-84e5-6ee6fc580757",
-		"0b281484-76cd-45a9-b6ce-68a0ea7f4b26",
-		"c56d95aa-e918-47ca-b102-486c9449fc4a",
-	]
+		'0213c8d4-f459-45ef-8bbc-96268ab56d01',
+		'04150e6e-b779-4125-84e5-6ee6fc580757',
+		'0b281484-76cd-45a9-b6ce-68a0ea7f4b26',
+		'c56d95aa-e918-47ca-b102-486c9449fc4a',
+	],
 };
 
 export const mockMaintenanceAlertsResponse = {
--- api/src/modules/maintenance-alerts/services/maintenance-alerts.service.spec.ts
+++ api/src/modules/maintenance-alerts/services/maintenance-alerts.service.spec.ts
@@ -1,44 +1,47 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { FindMaintenanceAlertByIdQuery, FindMaintenanceAlertsQuery, InsertMaintenanceAlertMutation } from "src/modules/shared/generated/graphql-db-types-hetarchief";
-import { DataService } from "../../data";
-import { TestingLogger } from "../../shared/logging/test-logger";
-import { MaintenanceAlertType } from "../maintenance-alerts.types";
-import { mockGqlMaintenanceAlert1, mockGqlMaintenanceAlert2, mockMaintenanceAlert1, mockUser } from "../mocks/maintenance-alerts.mocks";
-import { MaintenanceAlertsService } from "./maintenance-alerts.service";
+import { Test, TestingModule } from '@nestjs/testing';
+import {
+	DeleteMaintenanceAlertMutation,
+	FindMaintenanceAlertByIdQuery,
+	FindMaintenanceAlertsQuery,
+	InsertMaintenanceAlertMutation,
+	UpdateMaintenanceAlertMutation,
+} from 'src/modules/shared/generated/graphql-db-types-hetarchief';
+import { DataService } from '../../data';
+import { TestingLogger } from '../../shared/logging/test-logger';
+import { MaintenanceAlertType } from '../maintenance-alerts.types';
+import {
+	mockGqlMaintenanceAlert1,
+	mockGqlMaintenanceAlert2,
+	mockMaintenanceAlert1,
+} from '../mocks/maintenance-alerts.mocks';
+import { MaintenanceAlertsService } from './maintenance-alerts.service';
 
 const mockDataService: Partial<Record<keyof DataService, jest.SpyInstance>> = {
 	execute: jest.fn(),
 };
-/*
-	TODO:
-		findbyid
-		create
-		update
-		delete
-*/
 
 const getDefaultMaintenanceAlertsResponse = (): {
 	app_maintenance_alerts: FindMaintenanceAlertsQuery[];
-	app_maintenance_alerts_aggregate: { aggregate: { count: number }};
+	app_maintenance_alerts_aggregate: { aggregate: { count: number } };
 } => ({
 	app_maintenance_alerts: [mockGqlMaintenanceAlert1 as any],
 	app_maintenance_alerts_aggregate: {
 		aggregate: {
 			count: 100,
-		}
-	}
+		},
+	},
 });
 
 const getDefaultMaintenanceAlertsByIdResponse = (): {
 	app_maintenance_alerts: FindMaintenanceAlertByIdQuery[];
-	app_maintenance_alerts_aggregate: { aggregate: { count: number }};
+	app_maintenance_alerts_aggregate: { aggregate: { count: number } };
 } => ({
 	app_maintenance_alerts: [mockGqlMaintenanceAlert2 as any],
 	app_maintenance_alerts_aggregate: {
 		aggregate: {
 			count: 100,
-		}
-	}
+		},
+	},
 });
 
 describe('MaintenanceAlertsService', () => {
@@ -57,7 +60,9 @@ describe('MaintenanceAlertsService', () => {
 			.setLogger(new TestingLogger())
 			.compile();
 
-			maintenanceAlertsService = module.get<MaintenanceAlertsService>(MaintenanceAlertsService);
+		maintenanceAlertsService = module.get<MaintenanceAlertsService>(
+			MaintenanceAlertsService,
+		);
 	});
 
 	afterEach(() => {
@@ -66,7 +71,7 @@ describe('MaintenanceAlertsService', () => {
 
 	it('services should be defined', () => {
 		expect(maintenanceAlertsService).toBeDefined();
-	})
+	});
 
 	describe('adapt', () => {
 		it('can adapt a FindMaintenanceAlertsQuery hasura response to our maintenance alert class', () => {
@@ -82,7 +87,10 @@ describe('MaintenanceAlertsService', () => {
 		});
 
 		it('can adapt a personal FindMaintenanceAlertsQuery hasura response to our maintenance alert class', () => {
-			const adapted = maintenanceAlertsService.adapt(mockGqlMaintenanceAlert1, true);
+			const adapted = maintenanceAlertsService.adapt(
+				mockGqlMaintenanceAlert1,
+				true,
+			);
 
 			expect(adapted.id).toEqual(mockGqlMaintenanceAlert1.id);
 			expect(adapted.title).toEqual(mockGqlMaintenanceAlert1.title);
@@ -106,56 +114,60 @@ describe('MaintenanceAlertsService', () => {
 
 	describe('findAll', () => {
 		it('returns a paginated response with all maintenance alerts', async () => {
-			mockDataService.execute.mockResolvedValueOnce(getDefaultMaintenanceAlertsResponse())
-
-			const response = await maintenanceAlertsService.findAll(
-				{
-					page: 1,
-					size: 10,
-				}
+			mockDataService.execute.mockResolvedValueOnce(
+				getDefaultMaintenanceAlertsResponse(),
 			);
+
+			const response = await maintenanceAlertsService.findAll({
+				page: 1,
+				size: 10,
+			});
 			expect(response.items.length).toBe(1);
 			expect(response.page).toBe(1);
-			expect(response.size).toBe(10)
-			expect(response.total).toBe(100)
+			expect(response.size).toBe(10);
+			expect(response.total).toBe(100);
 		});
 
 		it('can filter on fromDate', async () => {
-			mockDataService.execute.mockResolvedValueOnce(getDefaultMaintenanceAlertsResponse())
-
-			const response = await maintenanceAlertsService.findAll(
-				{
-					fromDate: "2022-02-24T16:36:06.045845",
-					page: 1,
-					size: 10,
-				}
+			mockDataService.execute.mockResolvedValueOnce(
+				getDefaultMaintenanceAlertsResponse(),
 			);
+
+			const response = await maintenanceAlertsService.findAll({
+				fromDate: '2022-02-24T16:36:06.045845',
+				page: 1,
+				size: 10,
+			});
 			expect(response.items.length).toBe(1);
 			expect(response.items[0]?.fromDate).toBe(mockMaintenanceAlert1.fromDate);
 			expect(response.page).toBe(1);
-			expect(response.size).toBe(10)
-			expect(response.total).toBe(100)
+			expect(response.size).toBe(10);
+			expect(response.total).toBe(100);
 		});
 
 		it('can filter on untilDate', async () => {
-			mockDataService.execute.mockResolvedValueOnce(getDefaultMaintenanceAlertsResponse())
-
-			const response = await maintenanceAlertsService.findAll(
-				{
-					untilDate: "2022-02-27T16:36:06.045845",
-					page: 1,
-					size: 10,
-				}
+			mockDataService.execute.mockResolvedValueOnce(
+				getDefaultMaintenanceAlertsResponse(),
 			);
+
+			const response = await maintenanceAlertsService.findAll({
+				untilDate: '2022-02-27T16:36:06.045845',
+				page: 1,
+				size: 10,
+			});
 			expect(response.items.length).toBe(1);
-			expect(response.items[0]?.untilDate).toBe(mockMaintenanceAlert1.untilDate);
+			expect(response.items[0]?.untilDate).toBe(
+				mockMaintenanceAlert1.untilDate,
+			);
 			expect(response.page).toBe(1);
-			expect(response.size).toBe(10)
-			expect(response.total).toBe(100)
+			expect(response.size).toBe(10);
+			expect(response.total).toBe(100);
 		});
 
 		it('can filter on userGroups', async () => {
-			mockDataService.execute.mockResolvedValueOnce(getDefaultMaintenanceAlertsResponse())
+			mockDataService.execute.mockResolvedValueOnce(
+				getDefaultMaintenanceAlertsResponse(),
+			);
 
 			const response = await maintenanceAlertsService.findAll(
 				{
@@ -164,13 +176,13 @@ describe('MaintenanceAlertsService', () => {
 				},
 				{
 					userGroupIds: [
-						"0213c8d4-f459-45ef-8bbc-96268ab56d01",
-						"04150e6e-b779-4125-84e5-6ee6fc580757",
-						"0b281484-76cd-45a9-b6ce-68a0ea7f4b26",
-						"c56d95aa-e918-47ca-b102-486c9449fc4a"
+						'0213c8d4-f459-45ef-8bbc-96268ab56d01',
+						'04150e6e-b779-4125-84e5-6ee6fc580757',
+						'0b281484-76cd-45a9-b6ce-68a0ea7f4b26',
+						'c56d95aa-e918-47ca-b102-486c9449fc4a',
 					],
-					isPersonal: true
-				}
+					isPersonal: true,
+				},
 			);
 			expect(response.items.length).toBe(1);
 			expect(response.items[0]?.id).toBe(mockMaintenanceAlert1.id);
@@ -183,7 +195,9 @@ describe('MaintenanceAlertsService', () => {
 
 	describe('findById', () => {
 		it('returns a single maintenance alert', async () => {
-			mockDataService.execute.mockResolvedValueOnce(getDefaultMaintenanceAlertsByIdResponse());
+			mockDataService.execute.mockResolvedValueOnce(
+				getDefaultMaintenanceAlertsByIdResponse(),
+			);
 			const response = await maintenanceAlertsService.findById('1');
 			expect(response.id).toBe(mockGqlMaintenanceAlert2.id);
 		});
@@ -221,12 +235,62 @@ describe('MaintenanceAlertsService', () => {
 			const response = await maintenanceAlertsService.createMaintenanceAlert({
 				title: mockGqlMaintenanceAlert1.title,
 				message: mockGqlMaintenanceAlert1.message,
-				type: MaintenanceAlertType.QUESTION , //vragen aan brecht mockGqlMaintenanceAlert1.type
+				type: MaintenanceAlertType.QUESTION, //vragen aan brecht mockGqlMaintenanceAlert1.type
 				userGroups: mockGqlMaintenanceAlert1.user_groups,
 				fromDate: mockGqlMaintenanceAlert1.from_date,
 				untilDate: mockGqlMaintenanceAlert1.until_date,
-			})
+			});
 			expect(response.id).toBe(mockGqlMaintenanceAlert1.id);
+		});
+	});
+
+	describe('update', () => {
+		it('can update a new maintenance alert', async () => {
+			const mockData: UpdateMaintenanceAlertMutation = {
+				update_app_maintenance_alerts: {
+					returning: [mockGqlMaintenanceAlert1],
+				},
+			};
+			mockDataService.execute.mockResolvedValueOnce(mockData);
+
+			const response = await maintenanceAlertsService.updateMaintenanceAlert(
+				mockGqlMaintenanceAlert1.id,
+				{
+					title: 'Gepland onderhoud updated',
+					type: MaintenanceAlertType.QUESTION,
+				},
+			);
+			expect(response.id).toBe(mockGqlMaintenanceAlert1.id);
+		});
+	});
+
+	describe('delete', () => {
+		it('can delete a maintenance alert', async () => {
+			const mockData: DeleteMaintenanceAlertMutation = {
+				delete_app_maintenance_alerts: {
+					affected_rows: 1,
+				},
+			};
+			mockDataService.execute.mockResolvedValueOnce(mockData);
+
+			const affectedRows =
+				await maintenanceAlertsService.deleteMaintenanceAlert(
+					mockGqlMaintenanceAlert1.id,
+				);
+			expect(affectedRows).toBe(1);
+		});
+
+		it('can delete a non existing maintenance alert', async () => {
+			const mockData: DeleteMaintenanceAlertMutation = {
+				delete_app_maintenance_alerts: {
+					affected_rows: 0,
+				},
+			};
+			mockDataService.execute.mockResolvedValueOnce(mockData);
+
+			const affectedRows =
+				await maintenanceAlertsService.deleteMaintenanceAlert('unknown-id');
+			expect(affectedRows).toBe(0);
 		});
 	});
 });
