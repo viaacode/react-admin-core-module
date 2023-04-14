35b719f645cb8491a60bbaa2b7796fbdb0156883
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-04-14 11:44:39 +0200

    feat(BlockTagsWithLink): tweak translation keys

--- ui/src/react-admin/modules/content-page/components/blocks/BlockTagsWithLink/BlockTagsWithLink.editorconfig.ts
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockTagsWithLink/BlockTagsWithLink.editorconfig.ts
@@ -23,13 +23,13 @@ export const INITIAL_TAGS_WITH_LINK_BLOCK_STATE = (): DefaultContentBlockState =
 export const TAGS_WITH_LINK_BLOCK_CONFIG = (position = 0): ContentBlockConfig => ({
 	position,
 	name: AdminConfigManager.getConfig().services.i18n.tText(
-		'admin/content-block/helpers/generators/tags-with-link___title'
+		'admin/content-block/helpers/generators/tags-with-link___tags'
 	),
 	type: ContentBlockType.TagsWithLink,
 	components: {
 		state: INITIAL_TAGS_WITH_LINK_COMPONENTS_STATE(),
 		name: AdminConfigManager.getConfig().services.i18n.tText(
-			'admin/content-block/helpers/generators/tag-with-link___title'
+			'admin/content-block/helpers/generators/tag-with-link___tag'
 		),
 		fields: {
 			label: TEXT_FIELD(
