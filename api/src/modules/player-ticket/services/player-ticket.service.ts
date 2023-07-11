5a6234d665f563fbe283b7ba6f4905620351f7dc
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-07-11 18:54:29 +0200

    feat(avo-2740): clear cache of translations on edit
    https://meemoo.atlassian.net/browse/AVO-2740

--- api/src/modules/player-ticket/services/player-ticket.service.ts
+++ api/src/modules/player-ticket/services/player-ticket.service.ts
@@ -44,7 +44,7 @@ export class PlayerTicketService {
 
 	constructor(
 		@Inject(forwardRef(() => DataService)) protected dataService: DataService,
-		@Inject(CACHE_MANAGER) private cacheManager: Cache,
+		@Inject(CACHE_MANAGER) private cacheManager: Cache
 	) {
 		this.playerTicketsGotInstance = got.extend({
 			prefixUrl: process.env.TICKET_SERVICE_URL,
@@ -58,16 +58,13 @@ export class PlayerTicketService {
 			},
 		});
 		this.ticketServiceMaxAge = parseInt(
-			process.env.TICKET_SERVICE_MAXAGE || String(PLAYER_TICKET_EXPIRY),
+			process.env.TICKET_SERVICE_MAXAGE || String(PLAYER_TICKET_EXPIRY)
 		);
 		this.mediaServiceUrl = process.env.MEDIA_SERVICE_URL;
 		this.host = process.env.HOST;
 	}
 
-	protected async getToken(
-		path: string,
-		referer: string,
-	): Promise<PlayerTicket> {
+	protected async getToken(path: string, referer: string): Promise<PlayerTicket> {
 		const data = {
 			app: 'OR-*',
 			client: '', // TODO: Wait for reply on ARC-536 and implement resolution
@@ -75,19 +72,18 @@ export class PlayerTicketService {
 			maxage: this.ticketServiceMaxAge,
 		};
 
-		const playerTicket: PlayerTicket =
-			await this.playerTicketsGotInstance.get<PlayerTicket>(path, {
+		const playerTicket: PlayerTicket = await this.playerTicketsGotInstance.get<PlayerTicket>(
+			path,
+			{
 				searchParams: data,
 				resolveBodyOnly: true,
-			});
+			}
+		);
 
 		return playerTicket;
 	}
 
-	public async getPlayerToken(
-		embedUrl: string,
-		referer: string,
-	): Promise<string> {
+	public async getPlayerToken(embedUrl: string, referer: string): Promise<string> {
 		// no caching
 		const token = await this.getToken(embedUrl, referer);
 		return token.jwt;
@@ -96,16 +92,11 @@ export class PlayerTicketService {
 	public async getThumbnailToken(referer: string): Promise<string> {
 		const thumbnailPath = 'TESTBEELD/keyframes_all';
 
-		const options = {
-			ttl: (token) =>
-				differenceInSeconds(new Date(token.context.expiration), new Date()) -
-				60, // 60s margin to get the new token
-		};
 		try {
 			const token = await this.cacheManager.wrap(
 				`thumbnailToken-${referer}`,
 				() => this.getToken(thumbnailPath, referer),
-				options,
+				600_000 // 10 minutes
 			);
 			return token.jwt;
 		} catch (err) {
@@ -114,10 +105,7 @@ export class PlayerTicketService {
 		}
 	}
 
-	public async getPlayableUrl(
-		embedUrl: string,
-		referer: string,
-	): Promise<string> {
+	public async getPlayableUrl(embedUrl: string, referer: string): Promise<string> {
 		const token = await this.getPlayerToken(embedUrl, referer);
 
 		return `${this.mediaServiceUrl}/${embedUrl}?token=${token}`;
@@ -147,23 +135,17 @@ export class PlayerTicketService {
 
 		/* istanbul ignore next */
 		const browsePath: string =
-			(response as GetItemBrowsePathByExternalIdQuery)?.app_item_meta?.[0]
-				?.browse_path ||
-			(response as GetFileByRepresentationSchemaIdentifierQuery)
-				?.object_file?.[0]?.schema_embed_url;
+			(response as GetItemBrowsePathByExternalIdQuery)?.app_item_meta?.[0]?.browse_path ||
+			(response as GetFileByRepresentationSchemaIdentifierQuery)?.object_file?.[0]
+				?.schema_embed_url;
 		if (!browsePath) {
-			throw new NotFoundException(
-				`Object file with representation_id '${id}' not found`,
-			);
+			throw new NotFoundException(`Object file with representation_id '${id}' not found`);
 		}
 
 		return browsePath;
 	}
 
-	public async resolveThumbnailUrl(
-		path: string,
-		referer: string,
-	): Promise<string> {
+	public async resolveThumbnailUrl(path: string, referer: string): Promise<string> {
 		if (!path || !referer) {
 			return path;
 		}
