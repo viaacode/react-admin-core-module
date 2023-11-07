6decba6dda5c85c3730e58bc49f46affba4757ff
Bert Verhelst <verhelstbert@gmail.com>
2023-11-07 21:52:43 +0100

    fix(arc-1989): load correct styles for date picker

--- ui/src/react-admin/modules/shared/components/Timepicker/Timepicker.tsx
+++ ui/src/react-admin/modules/shared/components/Timepicker/Timepicker.tsx
@@ -5,7 +5,7 @@ import ReactDatePicker from 'react-datepicker';
 import { TimepickerProps } from './Timepicker.types';
 
 // This component only wraps in the styling
-import 'react-datepicker/dist/react-datepicker.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 // Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object.
 // https://github.com/Hacker0x01/react-datepicker/issues/3834#issuecomment-1451662259
