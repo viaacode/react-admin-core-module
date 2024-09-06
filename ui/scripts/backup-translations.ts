01d156716d91dd60778e0651134ba0e59c0c4472
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2024-09-06 11:09:54 +0200

    feat(translations): add backup script for qas translations

---
+++ ui/scripts/backup-translations.ts
@@ -0,0 +1,71 @@
+import { kebabCase } from 'lodash';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import {
+	App,
+	TranslationEntry,
+} from '../src/react-admin/modules/translations/translations.core.types';
+import { executeDatabaseQuery } from './execute-database-query';
+
+async function getOnlineTranslations(app: App): Promise<TranslationEntry[]> {
+	const response = await executeDatabaseQuery(
+		app,
+		`
+	query getAllTranslations {
+	  app_translations {
+	    component
+	    key
+	    language
+	    location
+	    value
+	    value_type
+	  }
+	}
+		`,
+		{},
+		'QAS' // Get translations from QAS
+	);
+	return response.data.app_translations.map((t: TranslationEntry) => ({ ...t, app }));
+}
+
+async function writeTranslationsToFile(
+	filePath: string,
+	translations: TranslationEntry[]
+): Promise<void> {
+	// Output all translations as sql file
+	let sql: string = translations
+		.map((translationEntry) => {
+			const component = `'${translationEntry.component}'`;
+			const location = `'${translationEntry.location}'`;
+			const key = `'${translationEntry.key}'`;
+			const value = `'${translationEntry.value.replace(/'/g, "''")}'`;
+			const value_type = `'${translationEntry.value_type}'`;
+			const language = `'${translationEntry.language}'`;
+			return `INSERT INTO app.translations ("component", "location", "key", "value", "value_type", "language") VALUES (${component}, ${location}, ${key}, ${value}, ${value_type}, ${language}) ON CONFLICT (component, location, key, language) DO UPDATE SET value = ${value}, value_type = ${value_type};`;
+		})
+		.sort()
+		.join('\n');
+	sql = 'TRUNCATE app.translations;\n' + sql;
+	await fs.writeFile(filePath, sql);
+	console.info('Finished writing ' + translations.length + ' translations');
+}
+
+async function backupTranslations(): Promise<void> {
+	const app = process.argv[2] as App;
+	if (app !== App.AVO && app !== App.HET_ARCHIEF) {
+		throw new Error(
+			'Backup translation script started with wrong "APP" parameter. Only valid values are: ["AVO", "HET_ARCHIEF"]'
+		);
+	}
+
+	const translations = await getOnlineTranslations(app);
+
+	const sqlFilePath = path.resolve(
+		`./data/${new Date().toISOString().split('T')[0]}-translations-${kebabCase(app)}-QAS-v3.sql`
+	);
+	await writeTranslationsToFile(sqlFilePath, translations);
+}
+
+backupTranslations().catch((err) => {
+	console.error('Backup translations failed: ', err);
+});
