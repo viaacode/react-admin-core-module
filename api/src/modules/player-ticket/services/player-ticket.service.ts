b2349a59dd20a198e290e064021bad5e528f008c
Bert Verhelst <verhelstbert@gmail.com>
2025-05-22 13:17:34 +0200

    4.4.4
    bump react-components

--- api/src/modules/player-ticket/services/player-ticket.service.ts
+++ api/src/modules/player-ticket/services/player-ticket.service.ts
@@ -70,10 +70,6 @@ export class PlayerTicketService {
 			maxage: this.ticketServiceMaxAge,
 		};
 
-		console.log('requesting ticket with ', data, {
-			url: process.env.TICKET_SERVICE_URL + '/' + path,
-		});
-
 		/**
 		 * Build the full URL from the base TICKET_SERVICE_URL and the path;
 		 * then append the query params from `data`.
