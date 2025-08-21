cc7ebd7d6f4bdbf1d40f1f58ddae6107056eb920
Bert Verhelst <verhelstbert@gmail.com>
2025-08-21 11:14:39 +0200

    feat(env): remove old environment variables

--- ui/src/react-admin/modules/translations/translations.types.ts
+++ ui/src/react-admin/modules/translations/translations.types.ts
@@ -1,5 +1,5 @@
-import type { ReactNode } from 'react';
 import type { DefaultProps } from '@viaa/avo2-components';
+import type { ReactNode } from 'react';
 import type {
 	Component,
 	Key,
@@ -7,13 +7,6 @@ import type {
 	Location,
 	ValueType,
 } from '~modules/translations/translations.core.types';
-
-export interface Translation {
-	key: string; // Key in translation object in the database
-	value: string; // Value of the translation in the translation object in the database
-	context: TranslationContextName; // Name of the config row that stores the translation object. eg: TRANSLATIONS_FRONTEND, TRANSLATIONS_BACKEND, TRANSLATIONS_ADMIN_CORE
-	label: string; // Display value in the application === context + '/' + key
-}
 
 export interface TranslationsOverviewProps extends DefaultProps {
 	renderPopup: (info: {
@@ -23,12 +16,6 @@ export interface TranslationsOverviewProps extends DefaultProps {
 		onSave: () => void;
 		onClose: () => void;
 	}) => ReactNode;
-}
-
-export enum TranslationContextName {
-	TRANSLATIONS_FRONTEND = 'TRANSLATIONS_FRONTEND',
-	TRANSLATIONS_BACKEND = 'TRANSLATIONS_BACKEND',
-	TRANSLATIONS_ADMIN_CORE = 'TRANSLATIONS_ADMIN_CORE',
 }
 
 export interface MultiLanguageTranslationEntry {
