f56d01c41d8004f5b6123d6f530d17606b025944
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-09-22 16:17:50 +0200

    AVO-37: change to radiobuttons

--- ui/src/react-admin/modules/content-page/components/ContentPagePreviewUserRoleSelector/ContentPagePreviewUserRoleSelector.tsx
+++ ui/src/react-admin/modules/content-page/components/ContentPagePreviewUserRoleSelector/ContentPagePreviewUserRoleSelector.tsx
@@ -1,15 +1,27 @@
 import { Button, Dropdown, DropdownButton, DropdownContent } from '@meemoo/react-components';
-import { Button as AvoButton, type DefaultProps, Icon, IconName } from '@viaa/avo2-components';
+import {
+	Button as AvoButton,
+	type DefaultProps,
+	Icon,
+	IconName,
+	RadioButtonGroup,
+} from '@viaa/avo2-components';
+import type { RadioOptionSchema } from '@viaa/avo2-components/src/components/RadioButtonGroup/RadioButtonGroup';
 import type { Avo } from '@viaa/avo2-types';
-import { isNil } from 'lodash-es';
+import { isNil, sortBy } from 'lodash-es';
 import React, { type FunctionComponent, useEffect, useMemo, useState } from 'react';
 import { StringParam, useQueryParams } from 'use-query-params';
-import { GET_SPECIAL_USER_GROUPS } from '~modules/user-group/const/user-group.const';
+import {
+	GET_ALL_CONTENT,
+	GET_LOGGED_OUT_USERS,
+	preferredUserGroupOrder,
+} from '~modules/user-group/const/user-group.const';
 import { useGetUserGroups } from '~modules/user-group/hooks/get-user-groups';
-import { UserGroupSelect } from '~shared/components/UserGroupSelect/UserGroupSelect';
 import { isAvo } from '~shared/helpers/is-avo';
 import { tText } from '~shared/helpers/translation-functions';
 import { SpecialPermissionGroups } from '~shared/types/authentication.types';
+
+import './ContentPagePreviewUserRoleSelector.scss';
 
 type ContentPagePreviewUserRoleSelectorProps = {
 	commonUser?: Avo.User.CommonUser;
@@ -19,110 +31,91 @@ export const ContentPagePreviewUserRoleSelector: FunctionComponent<
 	ContentPagePreviewUserRoleSelectorProps & DefaultProps
 > = (props) => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
-
 	const [queryParams, setQueryParams] = useQueryParams({
 		// Using stringParam so we can make a difference between none selected and missing parameter
-		userGroupIds: StringParam,
+		userGroupId: StringParam,
 	});
-
-	const { data: allUserGroupOptions } = useGetUserGroups({
-		withPermissions: false,
-	});
-
-	const selectedUserGroups = useMemo(
-		() => queryParams.userGroupIds?.split(',').filter((id) => !!id) || [],
-		[queryParams]
-	);
-	const buttonLabel = useMemo(() => {
-		if (!selectedUserGroups?.length) {
-			return tText('Preview als (geen selectie)');
-		}
-
-		if (selectedUserGroups?.length > 1) {
-			if (
-				selectedUserGroups.includes(SpecialPermissionGroups.loggedInUsers) &&
-				!selectedUserGroups.includes(SpecialPermissionGroups.loggedOutUsers)
-			) {
-				const selection = GET_SPECIAL_USER_GROUPS().find(
-					(item) => item.id === SpecialPermissionGroups.loggedInUsers
-				)?.label as string;
-
-				return tText('Preview als {{selectedUserGroup}}', { selectedUserGroup: selection });
-			}
-
-			return tText('Preview als meerdere gebruikersgroepen ({{count}})', {
-				count: selectedUserGroups.length.toString(),
-			});
-		}
-
-		const selection = [...(allUserGroupOptions || []), ...GET_SPECIAL_USER_GROUPS()].find(
-			(item) => item.id === selectedUserGroups[0]
-		)?.label as string;
-
-		return tText('Preview als {{selectedUserGroup}}', { selectedUserGroup: selection });
-	}, [allUserGroupOptions, selectedUserGroups]);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: Only run this once
 	useEffect(() => {
 		// if the queryParams are missing userGroupIds, use the userGroup of the current user
-		if (isNil(queryParams?.userGroupIds)) {
-			let currentUserGroupIds: string[];
-			if (props.commonUser?.userGroup?.id) {
-				currentUserGroupIds = [
-					String(props.commonUser?.userGroup?.id),
-					SpecialPermissionGroups.loggedInUsers,
-				];
-			} else {
-				currentUserGroupIds = [SpecialPermissionGroups.loggedOutUsers];
-			}
-
+		if (isNil(queryParams?.userGroupId)) {
 			setQueryParams({
-				userGroupIds: currentUserGroupIds.join(','),
+				userGroupId: String(
+					props.commonUser?.userGroup?.id
+						? props.commonUser?.userGroup?.id
+						: SpecialPermissionGroups.allContent
+				),
 			});
 		}
 	}, [props.commonUser, setQueryParams]);
 
+	const { data: userGroups, isLoading } = useGetUserGroups({
+		withPermissions: false,
+	});
+
+	const userGroupOptions = useMemo(() => {
+		if (isLoading) {
+			return [];
+		}
+		const allOptions = [
+			GET_ALL_CONTENT(),
+			GET_LOGGED_OUT_USERS(),
+			...sortBy(userGroups || [], (userGroup) => preferredUserGroupOrder[userGroup.label || '']),
+		];
+
+		return allOptions.map(
+			(opt): RadioOptionSchema => ({
+				label: opt.label as string,
+				value: opt.id as string,
+			})
+		);
+	}, [userGroups, isLoading]);
+
+	const buttonLabel = useMemo(() => {
+		const selection = (userGroupOptions || []).find(
+			(item) => item.value === queryParams.userGroupId
+		)?.label as string;
+
+		return tText('Preview als {{selectedUserGroup}}', { selectedUserGroup: selection });
+	}, [userGroupOptions, queryParams.userGroupId]);
+
 	return (
-		<>
-			<Dropdown
-				menuWidth="fit-content"
-				placement="bottom-end"
-				className={props.className}
-				isOpen={isMenuOpen}
-				onOpen={() => setIsMenuOpen(true)}
-				onClose={() => setIsMenuOpen(false)}
-			>
-				<DropdownButton>
-					{isAvo() ? (
-						<AvoButton
-							icon={IconName.eye}
-							type="borderless"
-							label={buttonLabel}
-							onClick={() => setIsMenuOpen(!isMenuOpen)}
-						/>
-					) : (
-						<Button
-							iconStart={<Icon name={IconName.eye} />}
-							label={buttonLabel}
-							variants="text"
-							onClick={() => setIsMenuOpen(!isMenuOpen)}
-						/>
-					)}
-				</DropdownButton>
-				<DropdownContent>
-					<UserGroupSelect
-						label=""
-						error={undefined}
-						values={queryParams?.userGroupIds?.split(',') || []}
-						required={false}
-						onChange={(userGroupIds: string[]) => {
-							setQueryParams({
-								userGroupIds: userGroupIds.join(','),
-							});
-						}}
+		<Dropdown
+			flyoutClassName="c-menu-content"
+			menuWidth="fit-content"
+			placement="bottom-end"
+			className={props.className}
+			isOpen={isMenuOpen}
+			onOpen={() => setIsMenuOpen(true)}
+			onClose={() => setIsMenuOpen(false)}
+		>
+			<DropdownButton>
+				{isAvo() ? (
+					<AvoButton
+						icon={IconName.eye}
+						type="borderless"
+						label={buttonLabel}
+						onClick={() => setIsMenuOpen(!isMenuOpen)}
 					/>
-				</DropdownContent>
-			</Dropdown>
-		</>
+				) : (
+					<Button
+						iconStart={<Icon name={IconName.eye} />}
+						label={buttonLabel}
+						variants="text"
+						onClick={() => setIsMenuOpen(!isMenuOpen)}
+					/>
+				)}
+			</DropdownButton>
+			<DropdownContent>
+				<RadioButtonGroup
+					options={userGroupOptions}
+					value={queryParams?.userGroupId || null}
+					onChange={(userGroupId: string) => {
+						setQueryParams({ userGroupId });
+					}}
+				/>
+			</DropdownContent>
+		</Dropdown>
 	);
 };
