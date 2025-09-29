7add979ac935cde8eb6b7a3471c654b1e939e640
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-09-29 11:12:17 +0200

    AVO-3564: detect size of page preview so the nudge is centered regardless of screen size

--- ui/src/react-admin/modules/content-page/components/blocks/BlockScrollDownNudge/BlockScrollDownNudge.tsx
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockScrollDownNudge/BlockScrollDownNudge.tsx
@@ -1,13 +1,34 @@
 import { Button, IconName } from '@viaa/avo2-components';
 import clsx from 'clsx';
-import React, { type FC, useEffect, useState } from 'react';
+import React, { type FC, useCallback, useEffect, useState } from 'react';
 
 import './BlockScrollDownNudge.scss';
 import { ROUTE_PARTS } from '~shared/consts';
 
 export const BlockScrollDownNudge: FC = () => {
-	const [visible, setVisible] = useState<boolean>(false);
 	const isAdminRoute = window.location.href.includes(ROUTE_PARTS.admin);
+
+	const [visible, setVisible] = useState<boolean>(false);
+	const [sizing, setSizing] = useState<
+		| {
+				width: number;
+				right: number;
+				left: number;
+		  }
+		| undefined
+	>(undefined);
+
+	const determinePosition = useCallback(() => {
+		const previewScrollable = document.querySelector('.c-content-page-preview');
+		if (previewScrollable) {
+			const contentRect = previewScrollable.getBoundingClientRect();
+			setSizing({
+				width: contentRect.width,
+				right: contentRect.right,
+				left: contentRect.left,
+			});
+		}
+	}, []);
 
 	useEffect(() => {
 		// Register window listener when the component mounts
@@ -28,11 +49,23 @@ export const BlockScrollDownNudge: FC = () => {
 		};
 	}, []);
 
+	useEffect(() => {
+		const previewScrollable = document.querySelector('.c-content-page-preview');
+		const resizeObserver = new ResizeObserver(determinePosition);
+
+		if (previewScrollable) {
+			resizeObserver.observe(previewScrollable);
+		}
+
+		determinePosition();
+	}, [determinePosition]);
+
 	return (
 		<div
 			className={clsx('c-nudge-scroll-down-wrapper', {
 				'c-nudge-scroll-down-wrapper--hidden': !isAdminRoute && !visible,
 			})}
+			style={sizing || {}}
 		>
 			<Button
 				className="c-nudge-scroll-down-button button-bounce"
