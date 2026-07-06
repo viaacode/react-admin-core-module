92ed0140143654732a6049e9e7208419287a1e69
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-07-06 14:27:53 +0200

    feat (LCM): Fix infinite render cycle

--- ui/src/react-admin/modules/content-page/components/blocks/BlockButtons/BlockButtons.wrapper.tsx
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockButtons/BlockButtons.wrapper.tsx
@@ -8,13 +8,19 @@ import { tText } from '~shared/helpers/translation-functions';
 export const BlockButtonsWrapper: FunctionComponent<BlockButtonsProps> = (props) => {
 	// Add tooltips for download buttons
 	const elements = props.elements;
-	elements.forEach((element) => {
+
+	const resolvedElements = elements.map((element) => {
 		if (element?.buttonAction?.type === 'FILE') {
-			element.tooltip = tText(
-				'admin/content-block/components/wrappers/buttons-wrapper/buttons-wrapper___gebruik-rechtermuisknop-en-kies-save-om-het-bestand-te-downloaden'
-			);
+			return {
+				...element,
+				tooltip: tText(
+					'admin/content-block/components/wrappers/buttons-wrapper/buttons-wrapper___gebruik-rechtermuisknop-en-kies-save-om-het-bestand-te-downloaden'
+				)
+			}
 		}
+
+		return element
 	});
 
-	return <BlockButtons {...props} elements={elements} />;
+	return <BlockButtons {...props} elements={resolvedElements} />;
 };
