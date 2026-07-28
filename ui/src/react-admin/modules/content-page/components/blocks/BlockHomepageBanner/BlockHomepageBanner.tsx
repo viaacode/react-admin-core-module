9ecc9f9ea4248091c34f7c0bb59e61f8c790cfbe
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-07-28 07:43:50 +0200

    ARC-3789: optimize pattern on desktop

--- ui/src/react-admin/modules/content-page/components/blocks/BlockHomepageBanner/BlockHomepageBanner.tsx
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockHomepageBanner/BlockHomepageBanner.tsx
@@ -4,13 +4,14 @@ import './BlockHomepageBanner.scss';
 import { Container } from '@viaa/avo2-components';
 import clsx from 'clsx';
 import { BlockHeading } from '~content-blocks/BlockHeading';
+import type { AlignOption } from '~modules/content-page/types/content-block.types';
 import { ContentPageWidth } from '~modules/content-page/types/content-pages.types.ts';
 import Html from '~shared/components/Html/Html.tsx';
 
 export interface BlockHomepageBannerProps extends DefaultComponentProps {
 	title: string;
 	content: string;
-	textAlign: string;
+	textAlign: AlignOption;
 	backgroundColor: string;
 	bannerColor: string;
 	pageWidth?: string;
@@ -26,7 +27,7 @@ export const BlockHomepageBanner: FunctionComponent<BlockHomepageBannerProps> = 
 }): ReactElement => {
 	return (
 		<article
-			className={clsx("c-block-homepage-banner", 'o-container')}
+			className={clsx('c-block-homepage-banner', 'o-container')}
 			style={
 				{
 					background: backgroundColor,
@@ -52,8 +53,14 @@ export const BlockHomepageBanner: FunctionComponent<BlockHomepageBannerProps> = 
 						{title}
 					</BlockHeading>
 					<Html className="c-block-homepage-banner__content-text" content={content} type="p"></Html>
-					<div className="c-block-homepage-banner__pattern c-block-homepage-banner__pattern--left" aria-hidden="true" />
-					<div className="c-block-homepage-banner__pattern c-block-homepage-banner__pattern--right" aria-hidden="true" />
+					<div
+						className="c-block-homepage-banner__pattern c-block-homepage-banner__pattern--left"
+						aria-hidden="true"
+					/>
+					<div
+						className="c-block-homepage-banner__pattern c-block-homepage-banner__pattern--right"
+						aria-hidden="true"
+					/>
 				</div>
 			</Container>
 		</article>
