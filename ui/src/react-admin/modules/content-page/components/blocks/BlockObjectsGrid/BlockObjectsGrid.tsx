78c1016cfcc46f33d75cc3844704b52e7eedfa4e
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-08-28 16:19:11 +0200

    ARC-3890: Fix 9/16 format for desktop

--- ui/src/react-admin/modules/content-page/components/blocks/BlockObjectsGrid/BlockObjectsGrid.tsx
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockObjectsGrid/BlockObjectsGrid.tsx
@@ -174,7 +174,7 @@ export const BlockObjectsGrid: FunctionComponent<BlockObjectsGridProps> = ({
 			className={clsx('c-block-objects-grid__tile', {
 				'c-block-objects-grid__tile--fixed': isFixed,
 			})}
-			key={`objects-grid-placeholder__${index}`}
+			key={`objects-grid-placeholder__${index}__${tileBackgroundColor}__${isFixed}`}
 			// The aria-live status below already announces that the objects are loading.
 			aria-hidden="true"
 		>
