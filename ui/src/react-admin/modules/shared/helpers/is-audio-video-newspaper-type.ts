5716e002b5d034cc46c3af458ed56a717a41b5f7
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-09-08 16:28:20 +0200

    ARC-3942: rename ie object type checks and export for reuse

--- ui/src/react-admin/modules/shared/helpers/is-audio-video-newspaper-type.ts
+++ ui/src/react-admin/modules/shared/helpers/is-audio-video-newspaper-type.ts
@@ -1,20 +1,20 @@
 import { type HetArchiefIeObjectType, HetArchiefSimpleIeObjectType } from '@viaa/avo2-types';
 import { mapDcTermsFormatToSimpleType } from '~shared/helpers/map-format-to-type.ts';
 
-export function isAudioType(format: HetArchiefIeObjectType | undefined): boolean {
+export function isAudioType(format: HetArchiefIeObjectType | undefined | null): boolean {
 	const simpleType = mapDcTermsFormatToSimpleType(format);
 	return simpleType === HetArchiefSimpleIeObjectType.AUDIO;
 }
 
-export function isVideoType(format: HetArchiefIeObjectType | undefined): boolean {
+export function isVideoType(format: HetArchiefIeObjectType | undefined | null): boolean {
 	const simpleType = mapDcTermsFormatToSimpleType(format);
 	return simpleType === HetArchiefSimpleIeObjectType.VIDEO;
 }
 
-export function isAudioVideoType(format: HetArchiefIeObjectType | undefined): boolean {
+export function isAudioVideoType(format: HetArchiefIeObjectType | undefined | null): boolean {
 	return isAudioType(format) || isVideoType(format);
 }
 
-export function isNewspaperType(format: HetArchiefIeObjectType | undefined): boolean {
+export function isNewspaperType(format: HetArchiefIeObjectType | undefined | null): boolean {
 	return mapDcTermsFormatToSimpleType(format) === HetArchiefSimpleIeObjectType.NEWSPAPER;
 }
