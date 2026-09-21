ad6d2b6cf0b808e69aa2d5f0e8d76cf6b812f100
Bert Verhelst <verhelstbert@gmail.com>
2026-09-21 17:52:23 +0200

    fix(ARC-3985): remove avo translations for aankeiler block
    https://meemoo.atlassian.net/browse/ARC-3985

--- ui/src/react-admin/modules/content-page/components/blocks/BlockHetArchiefAankeilerText/BlockHetArchiefAankeilerText.editorconfig.ts
+++ ui/src/react-admin/modules/content-page/components/blocks/BlockHetArchiefAankeilerText/BlockHetArchiefAankeilerText.editorconfig.ts
@@ -2,12 +2,12 @@ import type { RichTextEditorControl } from '@meemoo/react-components';
 import type { CheckboxProps } from '@viaa/avo2-components';
 
 import { tText } from '~shared/helpers/translation-functions';
+import { HET_ARCHIEF } from '~shared/types';
 import type {
 	ContentBlockConfig,
 	DefaultContentBlockState,
 } from '../../../types/content-block.types';
 import { Color, ContentBlockEditor, ContentBlockType } from '../../../types/content-block.types';
-
 import { BLOCK_FIELD_DEFAULTS, BLOCK_STATE_DEFAULTS, TEXT_FIELD } from '../defaults';
 import type { AankeilerTextBlockComponentState } from './BlockHetArchiefAankeilerText.types';
 
@@ -43,7 +43,9 @@ export const INITIAL_AANKEILER_TEXT_BLOCK_STATE = (): DefaultContentBlockState =
 
 export const AANKEILER_TEXT_BLOCK_CONFIG = (position = 0): ContentBlockConfig => ({
 	position,
-	name: tText('modules/content-page/components/blocks/block-aankeiler-text___aankeiler-tekst'),
+	name: tText('modules/content-page/components/blocks/block-aankeiler-text___aankeiler-tekst', {}, [
+		HET_ARCHIEF,
+	]),
 	type: ContentBlockType.AankeilerText,
 	components: {
 		state: INITIAL_AANKEILER_TEXT_COMPONENTS_STATE(),
@@ -55,7 +57,11 @@ export const AANKEILER_TEXT_BLOCK_CONFIG = (position = 0): ContentBlockConfig =>
 						controls: AANKEILER_TEXT_EDITOR_OPTIONS,
 					},
 				},
-				tText('modules/content-page/components/blocks/block-aankeiler-text___tekst-is-verplicht')
+				tText(
+					'modules/content-page/components/blocks/block-aankeiler-text___tekst-is-verplicht',
+					{},
+					[HET_ARCHIEF]
+				)
 			),
 		},
 	},
@@ -66,7 +72,9 @@ export const AANKEILER_TEXT_BLOCK_CONFIG = (position = 0): ContentBlockConfig =>
 				editorType: ContentBlockEditor.Checkbox,
 				editorProps: {
 					label: tText(
-						'modules/content-page/components/blocks/block-aankeiler-text___limiteer-breedte-tot-800-px'
+						'modules/content-page/components/blocks/block-aankeiler-text___limiteer-breedte-tot-800-px',
+						{},
+						[HET_ARCHIEF]
 					),
 				} as CheckboxProps,
 			},
