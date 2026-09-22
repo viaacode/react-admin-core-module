97541a8281e319b5e404b8b8043ca95e416f46c7
Bert Verhelst <verhelstbert@gmail.com>
2026-09-22 09:11:27 +0200

    fix(ARC-3948): allow rendering copyright icon without text
    https://meemoo.atlassian.net/browse/ARC-3948

--- ui/src/react-admin/modules/shared/components/CopyrightAttribution/CopyrightAttribution.tsx
+++ ui/src/react-admin/modules/shared/components/CopyrightAttribution/CopyrightAttribution.tsx
@@ -36,7 +36,7 @@ export const CopyrightAttribution: FunctionComponent<CopyrightAttributionProps> 
 
 	return (
 		<div className={clsx('a-copyright-attribution', className)}>
-			{title && renderTitle()}
+			{renderTitle()}
 			{text && (
 				<Html
 					content={text}
